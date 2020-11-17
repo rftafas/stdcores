a9a7a12f0619dcd88d5757f0cf1cb11e66f26983
Ricardo Tafas <rftafas@repodinamica.com.br>
2020-11-17 17:28:14 -0300

    Intercon MM compiles on Modelsim.

---
+++ aximm_intercon/aximm_intercon_pkg.vhd
@@ -0,0 +1,140 @@
+library IEEE;
+  use IEEE.STD_LOGIC_1164.ALL;
+  use IEEE.NUMERIC_STD.ALL;
+  use IEEE.math_real.all;
+library expert;
+  use expert.std_logic_expert.all;
+
+package aximm_intercon_pkg is
+
+  component aximm_intercon
+    generic (
+      master_portnum : positive := 8;
+      slave_portnum  : positive := 8;
+      DATA_BYTE_NUM  : positive := 8;
+      ADDR_SIZE      : positive := 8;
+      ID_WIDTH       : positive := 8
+    );
+    port (
+      rst_i         : in  std_logic;
+      clk_i         : in  std_logic;
+      addr_map_i    : in  std_logic_array(slave_portnum-1 downto 0)(ADDR_SIZE-1 downto 0);
+      M_AXI_AWID    : out std_logic_array (master_portnum-1 downto 0)(ID_WIDTH-1 downto 0);
+      M_AXI_AWVALID : out std_logic_vector(master_portnum-1 downto 0);
+      M_AXI_AWREADY : in  std_logic_vector(master_portnum-1 downto 0);
+      M_AXI_AWADDR  : out std_logic_array (master_portnum-1 downto 0)(ADDR_SIZE-1 downto 0);
+      M_AXI_AWPROT  : out std_logic_array (master_portnum-1 downto 0)(2 downto 0);
+      M_AXI_WVALID  : out std_logic_vector(master_portnum-1 downto 0);
+      M_AXI_WREADY  : in  std_logic_vector(master_portnum-1 downto 0);
+      M_AXI_WDATA   : out std_logic_array (master_portnum-1 downto 0)(8*DATA_BYTE_NUM-1 downto 0);
+      M_AXI_WSTRB   : out std_logic_array (master_portnum-1 downto 0)(DATA_BYTE_NUM-1 downto 0);
+      M_AXI_WLAST   : out std_logic_vector(master_portnum-1 downto 0);
+      M_AXI_BVALID  : in  std_logic_vector(master_portnum-1 downto 0);
+      M_AXI_BREADY  : out std_logic_vector(master_portnum-1 downto 0);
+      M_AXI_BRESP   : in  std_logic_array (master_portnum-1 downto 0)(1 downto 0);
+      M_AXI_BID     : in  std_logic_array (master_portnum-1 downto 0)(ID_WIDTH-1 downto 0);
+      M_AXI_ARVALID : out std_logic_vector(master_portnum-1 downto 0);
+      M_AXI_ARREADY : in  std_logic_vector(master_portnum-1 downto 0);
+      M_AXI_ARADDR  : out std_logic_array (master_portnum-1 downto 0)(ADDR_SIZE-1 downto 0);
+      M_AXI_ARPROT  : out std_logic_array (master_portnum-1 downto 0)(2 downto 0);
+      M_AXI_ARID    : out std_logic_array (master_portnum-1 downto 0)(ID_WIDTH-1 downto 0);
+      M_AXI_RVALID  : in  std_logic_vector(master_portnum-1 downto 0);
+      M_AXI_RREADY  : out std_logic_vector(master_portnum-1 downto 0);
+      M_AXI_RDATA   : in  std_logic_array (master_portnum-1 downto 0)(8*DATA_BYTE_NUM-1 downto 0);
+      M_AXI_RRESP   : in  std_logic_array (master_portnum-1 downto 0)(1 downto 0);
+      M_AXI_RID     : in  std_logic_array (master_portnum-1 downto 0)(ID_WIDTH-1 downto 0);
+      M_AXI_RLAST   : in  std_logic_vector(master_portnum-1 downto 0);
+      S_AXI_AWID    : in  std_logic_array (slave_portnum-1 downto 0)(ID_WIDTH-1 downto 0);
+      S_AXI_AWVALID : in  std_logic_vector(slave_portnum-1 downto 0);
+      S_AXI_AWREADY : out std_logic_vector(slave_portnum-1 downto 0);
+      S_AXI_AWADDR  : in  std_logic_array (slave_portnum-1 downto 0)(ADDR_SIZE-1 downto 0);
+      S_AXI_AWPROT  : in  std_logic_array (slave_portnum-1 downto 0)(2 downto 0);
+      S_AXI_WVALID  : in  std_logic_vector(slave_portnum-1 downto 0);
+      S_AXI_WREADY  : out std_logic_vector(slave_portnum-1 downto 0);
+      S_AXI_WDATA   : in  std_logic_array (slave_portnum-1 downto 0)(8*DATA_BYTE_NUM-1 downto 0);
+      S_AXI_WSTRB   : in  std_logic_array (slave_portnum-1 downto 0)(DATA_BYTE_NUM-1 downto 0);
+      S_AXI_WLAST   : in  std_logic_vector(slave_portnum-1 downto 0);
+      S_AXI_BVALID  : out std_logic_vector(slave_portnum-1 downto 0);
+      S_AXI_BREADY  : in  std_logic_vector(slave_portnum-1 downto 0);
+      S_AXI_BRESP   : out std_logic_array (slave_portnum-1 downto 0)(1 downto 0);
+      S_AXI_BID     : out std_logic_array (slave_portnum-1 downto 0)(ID_WIDTH-1 downto 0);
+      S_AXI_ARVALID : in  std_logic_vector(slave_portnum-1 downto 0);
+      S_AXI_ARREADY : out std_logic_vector(slave_portnum-1 downto 0);
+      S_AXI_ARADDR  : in  std_logic_array (slave_portnum-1 downto 0)(ADDR_SIZE-1 downto 0);
+      S_AXI_ARPROT  : in  std_logic_array (slave_portnum-1 downto 0)(2 downto 0);
+      S_AXI_ARID    : in  std_logic_array (slave_portnum-1 downto 0)(ID_WIDTH-1 downto 0);
+      S_AXI_RVALID  : out std_logic_vector(slave_portnum-1 downto 0);
+      S_AXI_RREADY  : in  std_logic_vector(slave_portnum-1 downto 0);
+      S_AXI_RDATA   : out std_logic_array (slave_portnum-1 downto 0)(8*DATA_BYTE_NUM-1 downto 0);
+      S_AXI_RRESP   : out std_logic_array (slave_portnum-1 downto 0)(1 downto 0);
+      S_AXI_RID     : out std_logic_array (slave_portnum-1 downto 0)(ID_WIDTH-1 downto 0);
+      S_AXI_RLAST   : out std_logic_vector(slave_portnum-1 downto 0)
+    );
+  end component aximm_intercon;
+
+  procedure set_peripheral_address (
+           port_number : in natural;
+           base_addr   : in std_logic_vector;
+    signal table       : inout std_logic_array
+  );
+
+  function set_peripheral_address (
+           port_number : natural;
+           base_addr   : std_logic_vector;
+           table       : std_logic_array
+  ) return std_logic_array;
+
+  function address_decode(
+           address     : std_logic_vector;
+           table       : std_logic_array
+  ) return natural;
+
+end package;
+
+package body aximm_intercon_pkg is
+
+  function set_peripheral_address (
+           port_number : natural;
+           base_addr   : std_logic_vector;
+           table       : std_logic_array
+  ) return std_logic_array is
+    variable tmp  : std_logic_array(table'range)(table(0)'range);
+  begin
+    assert base_addr'length = table(0)'length
+      report "Address size must be " & to_string(table(0)'length) & "bits."
+      severity failure;
+    tmp  := table;
+    for j in tmp'range loop
+      next when port_number = j;
+      assert ( not std_match(base_addr, tmp(j) ) )
+        report "Address conflict with peripheral: " & to_string(j) & ", base_address: " & to_string(tmp(j))
+        severity failure;
+    end loop;
+    tmp(port_number) := (others=>'0');
+    tmp(port_number) := base_addr;
+    return tmp;
+  end set_peripheral_address;
+
+  procedure set_peripheral_address (
+           port_number : in natural;
+           base_addr   : in std_logic_vector;
+    signal table       : inout std_logic_array
+  ) is
+  begin
+    table <= set_peripheral_address(port_number,base_addr,table);
+  end set_peripheral_address;
+
+  function address_decode (
+           address     : std_logic_vector;
+           table       : std_logic_array
+  ) return natural is
+    variable tmp : integer;
+  begin
+    for j in table'range loop
+      tmp := j;
+      exit when std_match( address, table(j) );
+    end loop;
+    return tmp;
+  end address_decode;
+
+end package body;
--- aximm_intercon/aximm_intercon_tb.vhd
+++ aximm_intercon/aximm_intercon_tb.vhd
@@ -17,17 +17,18 @@ library ieee;
   use ieee.numeric_std.all;
 library expert;
 	use expert.std_logic_expert.all;
+  use work.aximm_intercon_pkg.all;
 
 entity aximm_intercon_tb is
 end aximm_intercon_tb;
 
 architecture behavioral of aximm_intercon_tb is
 
-  constant master_portnum : positive := 8;
+  constant master_portnum : positive := 5;
   constant slave_portnum  : positive := 8;
   constant DATA_BYTE_NUM  : positive := 8;
-  constant ADDR_BYTE_NUM  : positive := 8;
-  constant ID_WIDTH       : positive := 8
+  constant ID_WIDTH       : positive := 8;
+  constant ADDR_SIZE      : positive := 8;
 
   --general
   signal rst_i         : std_logic := '0';
@@ -35,84 +36,107 @@ architecture behavioral of aximm_intercon_tb is
   --------------------------------------------------------------------------
   --AXIS Master Port
   --------------------------------------------------------------------------
-  signal M_AXI_AWID    : std_logic_array (master_portnum-1 downto 0,ID_WIDTH-1 downto 0);
+  signal M_AXI_AWID    : std_logic_array (master_portnum-1 downto 0)(ID_WIDTH-1 downto 0);
   signal M_AXI_AWVALID : std_logic_vector(master_portnum-1 downto 0);
   signal M_AXI_AWREADY : std_logic_vector(master_portnum-1 downto 0);
-  signal M_AXI_AWADDR  : std_logic_array (master_portnum-1 downto 0,8*ADDR_BYTE_NUM-1 downto 0);
-  signal M_AXI_AWPROT  : std_logic_array (master_portnum-1 downto 0,2 downto 0);
+  signal M_AXI_AWADDR  : std_logic_array (master_portnum-1 downto 0)(ADDR_SIZE-1 downto 0);
+  signal M_AXI_AWPROT  : std_logic_array (master_portnum-1 downto 0)(2 downto 0);
   --write data channel
   signal M_AXI_WVALID  : std_logic_vector(master_portnum-1 downto 0);
   signal M_AXI_WREADY  : std_logic_vector(master_portnum-1 downto 0);
-  signal M_AXI_WDATA   : std_logic_array (master_portnum-1 downto 0,8*DATA_BYTE_NUM-1 downto 0);
-  signal M_AXI_WSTRB   : std_logic_vector(master_portnum-1 downto 0,DATA_BYTE_NUM-1 downto 0);
+  signal M_AXI_WDATA   : std_logic_array (master_portnum-1 downto 0)(8*DATA_BYTE_NUM-1 downto 0);
+  signal M_AXI_WSTRB   : std_logic_array (master_portnum-1 downto 0)(DATA_BYTE_NUM-1 downto 0);
   signal M_AXI_WLAST   : std_logic_vector(master_portnum-1 downto 0);
   --Write Response channel
   signal M_AXI_BVALID  : std_logic_vector(master_portnum-1 downto 0);
   signal M_AXI_BREADY  : std_logic_vector(master_portnum-1 downto 0);
-  signal M_AXI_BRESP   : std_logic_array (master_portnum-1 downto 0,1 downto 0);
-  signal M_AXI_BID     : std_logic_array (master_portnum-1 downto 0,ID_WIDTH-1 downto 0);
+  signal M_AXI_BRESP   : std_logic_array (master_portnum-1 downto 0)(1 downto 0);
+  signal M_AXI_BID     : std_logic_array (master_portnum-1 downto 0)(ID_WIDTH-1 downto 0);
   -- Read Address channel
   signal M_AXI_ARVALID : std_logic_vector(master_portnum-1 downto 0);
   signal M_AXI_ARREADY : std_logic_vector(master_portnum-1 downto 0);
-  signal M_AXI_ARADDR  : std_logic_array (master_portnum-1 downto 0,8*ADDR_BYTE_NUM-1 downto 0);
-  signal M_AXI_ARPROT  : std_logic_array (master_portnum-1 downto 0,2 downto 0);
-  signal M_AXI_ARID    : std_logic_array (master_portnum-1 downto 0,ID_WIDTH-1 downto 0);
+  signal M_AXI_ARADDR  : std_logic_array (master_portnum-1 downto 0)(ADDR_SIZE-1 downto 0);
+  signal M_AXI_ARPROT  : std_logic_array (master_portnum-1 downto 0)(2 downto 0);
+  signal M_AXI_ARID    : std_logic_array (master_portnum-1 downto 0)(ID_WIDTH-1 downto 0);
   --Read data channel
   signal M_AXI_RVALID  : std_logic_vector(master_portnum-1 downto 0);
   signal M_AXI_RREADY  : std_logic_vector(master_portnum-1 downto 0);
-  signal M_AXI_RDATA   : std_logic_array (master_portnum-1 downto 0,8*DATA_BYTE_NUM-1 downto 0);
-  signal M_AXI_RRESP   : std_logic_array (master_portnum-1 downto 0,1 downto 0);
-  signal M_AXI_RID     : std_logic_array (master_portnum-1 downto 0,ID_WIDTH-1 downto 0);
+  signal M_AXI_RDATA   : std_logic_array (master_portnum-1 downto 0)(8*DATA_BYTE_NUM-1 downto 0);
+  signal M_AXI_RRESP   : std_logic_array (master_portnum-1 downto 0)(1 downto 0);
+  signal M_AXI_RID     : std_logic_array (master_portnum-1 downto 0)(ID_WIDTH-1 downto 0);
   signal M_AXI_RLAST   : std_logic_vector(master_portnum-1 downto 0);
   --------------------------------------------------------------------------
   --AXIS Slave Port
   --------------------------------------------------------------------------
-  signal S_AXI_AWID    : std_logic_array (slave_portnum-1 downto 0,ID_WIDTH-1 downto 0);
+  signal S_AXI_AWID    : std_logic_array (slave_portnum-1 downto 0)(ID_WIDTH-1 downto 0);
   signal S_AXI_AWVALID : std_logic_vector(slave_portnum-1 downto 0);
   signal S_AXI_AWREADY : std_logic_vector(slave_portnum-1 downto 0);
-  signal S_AXI_AWADDR  : std_logic_array (slave_portnum-1 downto 0,8*ADDR_BYTE_NUM-1 downto 0);
-  signal S_AXI_AWPROT  : std_logic_array (slave_portnum-1 downto 0,2 downto 0);
+  signal S_AXI_AWADDR  : std_logic_array (slave_portnum-1 downto 0)(ADDR_SIZE-1 downto 0);
+  signal S_AXI_AWPROT  : std_logic_array (slave_portnum-1 downto 0)(2 downto 0);
   --write data channel
   signal S_AXI_WVALID  : std_logic_vector(slave_portnum-1 downto 0);
   signal S_AXI_WREADY  : std_logic_vector(slave_portnum-1 downto 0);
-  signal S_AXI_WDATA   : std_logic_array (slave_portnum-1 downto 0,8*DATA_BYTE_NUM-1 downto 0);
-  signal S_AXI_WSTRB   : std_logic_vector(slave_portnum-1 downto 0,DATA_BYTE_NUM-1 downto 0);
+  signal S_AXI_WDATA   : std_logic_array (slave_portnum-1 downto 0)(8*DATA_BYTE_NUM-1 downto 0);
+  signal S_AXI_WSTRB   : std_logic_array (slave_portnum-1 downto 0)(DATA_BYTE_NUM-1 downto 0);
   signal S_AXI_WLAST   : std_logic_vector(slave_portnum-1 downto 0);
   --Write Response channel
   signal S_AXI_BVALID  : std_logic_vector(slave_portnum-1 downto 0);
   signal S_AXI_BREADY  : std_logic_vector(slave_portnum-1 downto 0);
-  signal S_AXI_BRESP   : std_logic_array (slave_portnum-1 downto 0,1 downto 0);
-  signal S_AXI_BID     : std_logic_array (slave_portnum-1 downto 0,ID_WIDTH-1 downto 0);
+  signal S_AXI_BRESP   : std_logic_array (slave_portnum-1 downto 0)(1 downto 0);
+  signal S_AXI_BID     : std_logic_array (slave_portnum-1 downto 0)(ID_WIDTH-1 downto 0);
   -- Read Address channel
   signal S_AXI_ARVALID : std_logic_vector(slave_portnum-1 downto 0);
   signal S_AXI_ARREADY : std_logic_vector(slave_portnum-1 downto 0);
-  signal S_AXI_ARADDR  : std_logic_array (slave_portnum-1 downto 0,8*ADDR_BYTE_NUM-1 downto 0);
-  signal S_AXI_ARPROT  : std_logic_array (slave_portnum-1 downto 0,2 downto 0);
-  signal S_AXI_ARID    : std_logic_array (slave_portnum-1 downto 0,ID_WIDTH-1 downto 0);
+  signal S_AXI_ARADDR  : std_logic_array (slave_portnum-1 downto 0)(ADDR_SIZE-1 downto 0);
+  signal S_AXI_ARPROT  : std_logic_array (slave_portnum-1 downto 0)(2 downto 0);
+  signal S_AXI_ARID    : std_logic_array (slave_portnum-1 downto 0)(ID_WIDTH-1 downto 0);
   --Read data channel
   signal S_AXI_RVALID  : std_logic_vector(slave_portnum-1 downto 0);
   signal S_AXI_RREADY  : std_logic_vector(slave_portnum-1 downto 0);
-  signal S_AXI_RDATA   : std_logic_array (slave_portnum-1 downto 0,8*DATA_BYTE_NUM-1 downto 0);
-  signal S_AXI_RRESP   : std_logic_array (slave_portnum-1 downto 0,1 downto 0);
-  signal S_AXI_RID     : std_logic_array (slave_portnum-1 downto 0,ID_WIDTH-1 downto 0);
-  signal S_AXI_RLAST   : std_logic_vector(slave_portnum-1 downto 0)
+  signal S_AXI_RDATA   : std_logic_array (slave_portnum-1 downto 0)(8*DATA_BYTE_NUM-1 downto 0);
+  signal S_AXI_RRESP   : std_logic_array (slave_portnum-1 downto 0)(1 downto 0);
+  signal S_AXI_RID     : std_logic_array (slave_portnum-1 downto 0)(ID_WIDTH-1 downto 0);
+  signal S_AXI_RLAST   : std_logic_vector(slave_portnum-1 downto 0);
+
+  signal addr_map_s : std_logic_array(master_portnum-1 downto 0)(ADDR_SIZE-1 downto 0) := (others=>(others=>'X'));
+  signal addr_map_c : std_logic_array(master_portnum-1 downto 0)(ADDR_SIZE-1 downto 0) := (
+    ("0000----"),
+    ("0001----"),
+    ("0010----"),
+    ("01------"),
+    ("1-------")
+  );
 
 begin
 
 clk_i <= not clk_i after 10 ns;
 rst_i <= '1',  '0' after 10 ns;
 
+
+--There are several ways to configure the peripheral address map. It can be made
+-- on signal declaration (see addr_map_c above) or using the set_peripheral_api.
+config_process : process(all)
+begin
+  set_peripheral_address(0,"0000----",addr_map_s);
+  set_peripheral_address(1,"0001----",addr_map_s);
+  set_peripheral_address(2,"0010----",addr_map_s);
+  set_peripheral_address(3,"01------",addr_map_s);
+  set_peripheral_address(4,"1-------",addr_map_s);
+end process;
+
 aximm_intercon_i : aximm_intercon
   generic map (
     master_portnum => master_portnum,
     slave_portnum  => slave_portnum,
     DATA_BYTE_NUM  => DATA_BYTE_NUM,
-    ADDR_BYTE_NUM  => ADDR_BYTE_NUM,
+    ADDR_SIZE      => ADDR_SIZE,
     ID_WIDTH       => ID_WIDTH
   )
   port map (
     rst_i         => rst_i,
     clk_i         => clk_i,
+    addr_map_i    => addr_map_s,
+
     M_AXI_AWID    => M_AXI_AWID,
     M_AXI_AWVALID => M_AXI_AWVALID,
     M_AXI_AWREADY => M_AXI_AWREADY,
